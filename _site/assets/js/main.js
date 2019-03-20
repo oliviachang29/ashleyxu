566b6e8011bfa1c9ef4555292a790ed4db98fc6a
Olivia <cozymonster29@gmail.com>
2019-03-19 18:47:50 -0700

    change client font size

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,5 +1,17 @@
-// $('#fullpage').fullpage({
+$('#fullpage').fullpage({
 // 		//options here
 // 		// autoScrolling:true,
-// 		// scrollHorizontally: true
-// 	});
+	scrollHorizontally: true
+});
+
+// $(document).on('scroll', function() {
+//     if($(this).scrollTop()>=$('#contact').position().top){
+//         $('.nav-color-control').css({
+//         	color: 'black !important'
+//         })
+//     } else {
+//     	$('.nav-color-control').css({
+//         	color: 'white !important'
+//         })
+//     }
+// })
