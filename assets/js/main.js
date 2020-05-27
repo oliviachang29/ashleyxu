b0c611491bde80e2769ae778fb5c8322aaec680f
Olivia <15709895+oliviachang29@users.noreply.github.com>
2020-05-27 15:13:56 -0700

    change hero looks super frickin cool

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,28 @@
-$('img').unveil({
+const PAGE_HEIGHT = $(document).height() - $(window).height()
+
+$(window).scroll(function(){
+	var strikethrough = $("#strikethrough");
+	var width = ((1 - ($(window).scrollTop() / PAGE_HEIGHT)) * 100).toString() + "%";
+	console.log(width)
+	strikethrough.css("width", width);
+});
+
+$('.lazy').unveil({
       offset: 200,
       placeholder: '/assets/images/lazy.jpg',
       debug: true
   }).on('loaded.unveil', function () {
       this.style.opacity = 1;
   });
+
+var figure = $(".singlevidjs").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play();
+    $('video', this).css("filter", "brightness(0.7)")
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
+    $('video', this).css("filter", "brightness(0.5)")
+}
