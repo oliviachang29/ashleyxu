c73694c67f11c5fba2556481ec107816506dd4cc
Olivia <15709895+oliviachang29@users.noreply.github.com>
2020-05-28 12:47:15 -0700

    progress towards mobile friendly and change background text on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,61 @@
+function calculateVisibilityForDiv(div$) {
+    var windowHeight = $(window).height(),
+        docScroll = $(document).scrollTop(),
+        divPosition = div$.offset().top,
+        divHeight = div$.height(),
+        hiddenBefore = docScroll - divPosition,
+        hiddenAfter = (divPosition + divHeight) - (docScroll + windowHeight);
+
+    if ((docScroll > divPosition + divHeight) || (divPosition > docScroll + windowHeight)) {
+        return 0;
+    } else {
+        var result = 100;
+
+        if (hiddenBefore > 0) {
+            result -= (hiddenBefore * 100) / divHeight;
+        }
+
+        if (hiddenAfter > 0) {
+            result -= (hiddenAfter * 100) / divHeight;
+        }
+
+        return result;
+    }
+}
+
 function initJS() {
   const PAGE_HEIGHT = $(document).height() - $(window).height()
   $(window).scroll(function(){
-    var strikethrough = $("#strikethrough");
-    var width = ((1 - ($(window).scrollTop() / PAGE_HEIGHT)) * 100).toString() + "%";
-    strikethrough.css("width", width);
+    // line becomes shorter as user scrolls
+    // var strikethrough = $("#strikethrough");
+    // var width = ((1 - ($(window).scrollTop() / PAGE_HEIGHT)) * 100).toString() + "%";
+    // strikethrough.css("width", width);
+
+    var results = []
+
+    $('section').each(function () {
+        var div$ = $(this);
+        results.push({
+          id: div$.attr('id'),
+          backgroundText: div$.attr('data-background-text'),
+          visibility: Math.round(calculateVisibilityForDiv(div$))
+        })
+    });
+
+    var mostVisible = {visibility: 0};
+    results.forEach(function(item, index) {
+      if (item.visibility > mostVisible.visibility) {
+        mostVisible = item;
+      }
+    })
+
+    if ($('.background-text').first().text() !== mostVisible.backgroundText) {
+      $('.background-text').text(mostVisible.backgroundText);
+    }
+
   });
 
+  // unveil images
   $('.lazy').unveil({
         offset: 200,
         placeholder: '/assets/images/lazy.jpg',
@@ -14,6 +64,7 @@ function initJS() {
         this.style.opacity = 1;
     });
 
+  // play videos on hover
   var figure = $(".singlevidjs").hover( hoverVideo, hideVideo );
   function hoverVideo(e) {  
       $('video', this).get(0).play();
