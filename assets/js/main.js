43dc2a1418558f47373606bed8a2d9a9bf5afef5
Olivia <15709895+oliviachang29@users.noreply.github.com>
2020-05-30 12:34:29 -0700

    use unveil.js instead of unveil2.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,88 +6,87 @@ var players = {}
 var player_id = ''
 
 function homePage() {
-  if (players[player_id]) {
-    players[player_id].dispose()
-  }
-
-  $(window).scroll(function(){
-    if (!isProjectHover) {
-      var results = []
-
-      $('section').each(function () {
-          var div$ = $(this);
-          results.push({
-            id: div$.attr('id'),
-            backgroundText: div$.attr('data-background-text'),
-            visibility: Math.round(calculateVisibilityForDiv(div$))
-          })
-      });
-
-      // set background text based on the most visible div
-      var mostVisible = {visibility: 0};
-      results.forEach(function(item, index) {
-        if (item.visibility > mostVisible.visibility) {
-          mostVisible = item;
-        }
-      })
-
-      setBackgroundText(mostVisible.backgroundText)
+    if (players[player_id]) {
+        players[player_id].dispose()
     }
 
-  });
+    // don't load videos on mobile
+    if (!jQuery.browser.mobile) {
+        $('.no-load-mobile').each(function() {
+            $('source', this).each(function() {
+                $(this).attr('src', $(this).attr('data-src'))
+            })
+            $(this).load()
+        })
+        $(window).scroll(function() {
+            if (!isProjectHover) {
+                var results = []
 
-  // play videos on hover
-  var figure = $(".singlevidjs").hover( hoverVideo, hideVideo );
+                $('section').each(function() {
+                    var div$ = $(this);
+                    results.push({
+                        id: div$.attr('id'),
+                        backgroundText: div$.attr('data-background-text'),
+                        visibility: Math.round(calculateVisibilityForDiv(div$))
+                    })
+                });
 
-  // don't load videos on mobile
-  if (!jQuery.browser.mobile) {
-    $('.no-load-mobile').each(function() {
-      $('source', this).each(function() {
-        $(this).attr('src', $(this).attr('data-src'))
-      })
-      $(this).load()
-    })
-  }
+                // set background text based on the most visible div
+                var mostVisible = { visibility: 0 };
+                results.forEach(function(item, index) {
+                    if (item.visibility > mostVisible.visibility) {
+                        mostVisible = item;
+                    }
+                })
+
+                setBackgroundText(mostVisible.backgroundText)
+            }
+
+        });
+
+        // play videos on hover
+        var figure = $(".singlevidjs").hover(hoverVideo, hideVideo);
+    }
 
 }
 
-function hoverVideo(e) {  
-  $('video', this).get(0).play();
-  $('video', this).css("filter", "brightness(0.7)")
-  isProjectHover = true;
-  $('html, body').css("background-color", $(this).attr("data-primary-color"))
-  setBackgroundText($(this).attr("data-background-text"), true)
+function hoverVideo(e) {
+    $('video', this).get(0).play();
+    $('video', this).css("filter", "brightness(0.7)")
+    isProjectHover = true;
+    $('html, body').css("background-color", $(this).attr("data-primary-color"))
+    setBackgroundText($(this).attr("data-background-text"), true)
 
-  $('.marquee-inner').css('animation-duration', '7s')
+    $('.marquee-inner').css('animation-duration', '7s')
 }
 
 function hideVideo(e) {
-  $('video', this).get(0).pause(); 
-  $('video', this).css("filter", "brightness(0.5)")
-  isProjectHover = false;
-  setBackgroundText(previousBackgroundText)
+    $('video', this).get(0).pause();
+    $('video', this).css("filter", "brightness(0.5)")
+    isProjectHover = false;
+    setBackgroundText(previousBackgroundText)
 
-  // uncomment to change color back to default
-  // $('html, body').css("background-color", defaultBackgroundColor)
-  
-  $('.marquee-inner').css('animation-duration', '40s')
+    // uncomment to change color back to default
+    // $('html, body').css("background-color", defaultBackgroundColor)
+
+    $('.marquee-inner').css('animation-duration', '40s')
 }
 
-function setBackgroundText(text, italic=false) {
-  // check if the text isn't already that
-  if ($('.background-text').first().text() !== text) {
-    // set the previous text so we can revert
-    previousBackgroundText = $('.background-text').first().text();
-    // replace the text
-    $('.background-text').text(text.replace(/\W/g, ''));
+function setBackgroundText(text, italic = false) {
+    // check if the text isn't already that
+    if ($('.background-text').first().text() !== text) {
+        // set the previous text so we can revert
+        previousBackgroundText = $('.background-text').first().text();
+        // replace the text
+        $('.background-text').text(text.replace(/\W/g, ''));
 
-    if (italic) {
-      // italicize background text
-      $('.background-text').css("font-style", "italic")
-    } else {
-      $('.background-text').css("font-style", "normal")
+        if (italic) {
+            // italicize background text
+            $('.background-text').css("font-style", "italic")
+        } else {
+            $('.background-text').css("font-style", "normal")
+        }
     }
-  }
 }
 
 // calculate what percent of a div is visible
@@ -117,24 +116,32 @@ function calculateVisibilityForDiv(div$) {
 }
 
 function projectPage() {
-  var player_id = $('.project-page-video').attr('id')
-  players[player_id] = videojs(player_id)
+    var player_id = $('.project-page-video').attr('id')
+    players[player_id] = videojs(player_id)
+}
+
+function unveilImages() {
+	// $('.lazy').unveil({
+	//   offset: 200,
+	//   placeholder: '/assets/images/lazy.jpg',
+	//   debug: true
+	// }).on('loaded.unveil', function() {
+	//   this.style.opacity = 1;
+	// });
+    
+	$(".lazy").unveil(100, function() {
+    $(this).on('load', function() {
+      this.style.opacity = 1;
+    });
+  });
 }
 
 function pageHandler() {
-  // unveil images
-  $('.lazy').unveil({
-      offset: 200,
-      placeholder: '/assets/images/lazy.jpg',
-      debug: true
-  }).on('loaded.unveil', function () {
-      this.style.opacity = 1;
-  });
-  
+	unveilImages()
   if (window.location.pathname == '/') {
-    homePage()
+      homePage()
   } else {
-    projectPage()
+      projectPage()
   }
 }
 
@@ -142,7 +149,7 @@ function pageHandler() {
 $(function() {
     const swup = new Swup();
     swup.on('contentReplaced', function() {
-      pageHandler()
+        pageHandler()
     });
 });
 
